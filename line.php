1b25c0636f0c3f4f4b779b766aaca530f486ab34
eakawats <38580850+eakawats@users.noreply.github.com>
2018-12-04 23:54:29 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'Zw7ySd1liYsxC91pY4b6mjCJxWjAX2tLh2ukn8by51Y6AKFpvoU6js5QfKu7HMUICkp65Bfyitna3EwtjLgdejDMEGeMarxuHSRkwEfmV3alfGfdAcPMpQX1Dew0K4ED5l8ai2+0mta1jCrpdU+T7wdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U2bd74eb0be75d00c39481888ef57a6cb',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
