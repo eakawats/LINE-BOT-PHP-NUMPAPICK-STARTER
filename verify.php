3e3fb337f47e699d56444a0c679a1c9cd8569f73
eakawats <38580850+eakawats@users.noreply.github.com>
2018-12-04 23:58:43 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,6 @@
 <?php
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+//$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'Zw7ySd1liYsxC91pY4b6mjCJxWjAX2tLh2ukn8by51Y6AKFpvoU6js5QfKu7HMUICkp65Bfyitna3EwtjLgdejDMEGeMarxuHSRkwEfmV3alfGfdAcPMpQX1Dew0K4ED5l8ai2+0mta1jCrpdU+T7wdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
