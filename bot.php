35a1a702a80b72339bcf259320da084727f4b09d
eakawats <38580850+eakawats@users.noreply.github.com>
2018-12-12 00:45:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,7 +6,7 @@
 $content = file_get_contents('php://input');
 // Parse JSON
 
-$events = json_decode($content, ON);
+$events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['ESP'])) {
 	
